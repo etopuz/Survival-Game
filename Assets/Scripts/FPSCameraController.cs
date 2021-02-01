3cfc6e2c333298b349fddbbedfe45c077bd2e0aa
Eyyüp Ensar Topuz <b21827905@cs.hacettepe.edu.tr>
2021-02-01 17:30:07 +0300

    added

---
+++ Assets/Scripts/FPSCameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCameraController : MonoBehaviour
+{
+    private float mouseSensitivity = 100f;
+    private Transform playerBody;
+    private float xRotation = 0f;
+
+    void Awake()
+    {
+        playerBody = GameObject.FindGameObjectWithTag("Player").transform;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        playerBody.Rotate(Vector3.up * mouseX);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    
+    [Header("Control")]
+    private CharacterController controller;
+    private Vector3 velocity;
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+    private float gravity = -9.81f;
+    [SerializeField]
+    private float jumpHeight = 3f;
+
+    [Header("Checking Is Grounded")]
+    private Transform groundCheck;
+    private LayerMask groundMask;
+    [SerializeField]
+    private float groundDistance = 0.42f;
+    [SerializeField]
+    bool isGrounded;
+
+    private Animator playerAnim;
+    private bool isAttacking = true;
+
+
+    private void Awake()
+    {
+        playerAnim = transform.Find("remy").GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        groundMask = LayerMask.GetMask("Ground");
+        groundCheck = transform.Find("GroundCheck");
+    }
+
+    void Update()
+    {
+        CheckIsGrounded();
+        Move();
+        Jump();
+        Gravity();
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            playerAnim.SetBool("isAttacking", isAttacking);
+            //isAttacking = false;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+    private void CheckIsGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+    }
+
+    private void Gravity()
+    {
+        velocity.y += gravity * Time.deltaTime * 2;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+
+}
